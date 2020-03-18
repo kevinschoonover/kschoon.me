a5f21c8921a55a4f8d03d321c53be5d01c53ef5d
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-03-18 22:52:55 +0000

    Add basic terraform configuration with nomad deployment via ansible

--- deploy/main.tf
+++ deploy/main.tf
@@ -9,13 +9,19 @@ resource "digitalocean_ssh_key" "default" {
 
 resource "digitalocean_droplet" "primary_api" {
   name = "api.kschoon.me"
-  image = "ubuntu-18-04-x64"
+  # image = "ubuntu-18-04-x64"
+  image = "docker-18-04"
   region = "nyc3"
   size = "s-1vcpu-1gb"
   monitoring = true
   private_networking = true
-  tags = ["api"]
+  tags = ["api", "nomad_instances"]
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+}
+
+resource "digitalocean_domain" "api" {
+  name       = "api.kschoon.me"
+  ip_address = digitalocean_droplet.primary_api.ipv4_address
 }
 
 resource "digitalocean_project" "default" {
@@ -23,5 +29,5 @@ resource "digitalocean_project" "default" {
   description = "All *.kschoon.me services and infrastructure"
   purpose = "Other"
   environment = terraform.workspace == "production" ? "production" : "development"
-  resources   = [digitalocean_droplet.primary_api.urn]
+  resources   = [digitalocean_droplet.primary_api.urn, digitalocean_domain.api.urn]
 }
