d978ba169e5ef8f12b382aa3474e9d900219a761
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-03-21 16:51:48 +0000

    Add async TestClient + test showing specific checkins

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
+from async_asgi_testclient import TestClient as AsyncTestClient
+
 
 from app.main import app
 from app.database import create_database
@@ -23,10 +25,22 @@ def patch_docker(monkeypatch):
 @pytest.fixture
 def database(monkeypatch):
     created_db = create_database(force_rollback=True)
-    return monkeypatch.setattr("app.main.database", created_db)
+    return created_db, monkeypatch.setattr("app.main.database", created_db)
 
 
 @pytest.fixture(scope="module")
-def test_app():
+def sync_client():
     client = TestClient(app)
     yield client  # testing happens here
+
+
+# The async client is necessary when I want to insert something in the database
+# using encode/databases because the standard Starlette / FastAPI TestClient
+# does not support for asynchronous context manager i.e.:
+# `async with TestClient(app) as client:` that is necessary to run the client in
+# a async def function. This can potentially change by the test client being
+# updated in starlette using httpx. Tracked at https://github.com/encode/starlette/issues/652
+@pytest.fixture(scope="function")
+async def async_client():
+    client = AsyncTestClient(app)
+    yield client
