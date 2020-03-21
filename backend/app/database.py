c4fdd79ee5fe54dc3aa111efc2e96543d6a67759
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-03-21 17:19:11 +0000

    Update typings + add Jedi for autocompletion

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,22 +3,22 @@ import os
 import databases
 import sqlalchemy
 
-POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
-POSTGRES_PASS = os.environ.get("POSTGRES_PASS", "postgres")
+POSTGRES_DB: str = os.environ.get("POSTGRES_DB", "postgres")
+POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_USER: str = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_PASS: str = os.environ.get("POSTGRES_PASS", "postgres")
 
-DATABASE_URL = (
+DATABASE_URL: str = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}/{POSTGRES_DB}"
 )
 
 
-def create_database(force_rollback=False):
+def create_database(force_rollback=False) -> databases.Database:
     return databases.Database(DATABASE_URL, force_rollback=force_rollback)
 
 
-database = create_database()
+database: databases.Database = create_database()
 
-metadata = sqlalchemy.MetaData()
+metadata: sqlalchemy.MetaData = sqlalchemy.MetaData()
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
