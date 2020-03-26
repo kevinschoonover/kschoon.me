1ba37821eec7cbb0f1bc8ea6b2e3a5875bec9b6b
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-03-26 00:32:09 +0000

    Add basic implementation for submitting checkins

--- checkin/src/config.ts
+++ checkin/src/config.ts
@@ -1,9 +1,16 @@
 export interface IConfig {
   API_URI: string;
+  CHECKIN_URL: string;
 }
 
+const API_URI =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost"
+    : "https://api.kschoon.me";
+
 const config: IConfig = {
-  API_URI: "http://localhost/",
+  API_URI,
+  CHECKIN_URL: `${API_URI}/checkins`,
 };
 
 export { config };
