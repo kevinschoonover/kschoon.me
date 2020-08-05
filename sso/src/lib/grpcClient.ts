43662f7b18651fe7709f90abca6216cf085699bd
Kevin Schoonover <me@kschoon.me>
2020-08-05 15:01:33 -0700

    Refactor the sso service

--- sso/src/lib/grpcClient.ts
+++
@@ -1,8 +0,0 @@
-import * as grpc from "@grpc/grpc-js";
-
-import { IdentityClient } from "kschoonme-identity-pb";
-
-export const client = new IdentityClient(
-  `localhost:3009`,
-  grpc.credentials.createInsecure()
-);
