7584b713cab48dc10059027f608a7fe65a478a8d
Kevin Schoonover <me@kschoon.me>
2020-08-11 15:29:56 -0700

    Add redis, sso, and identity services to docker-compose

--- identity/src/config.ts
+++ identity/src/config.ts
@@ -1,7 +1,11 @@
 export interface IConfig {
   GRPC_PORT: string;
+  POSTGRES_URL: string;
 }
 
+const { GRPC_PORT, POSTGRES_URL } = process.env;
+
 export const config: IConfig = {
-  GRPC_PORT: process.env.GRPC_PORT || "3009",
+  GRPC_PORT: GRPC_PORT || "3009",
+  POSTGRES_URL: POSTGRES_URL || "postgresql://postgres:postgres@localhost",
 };
--- sso/src/config.ts
+++ sso/src/config.ts
@@ -2,14 +2,19 @@ interface IConfig {
   ISSUER: string;
   PORT: number;
   IS_PRODUCTION: boolean;
+  REDIS_URL: string;
+  USE_REDIS: boolean;
 }
 
-const { PORT, ISSUER, NODE_ENV } = process.env;
+const { PORT, ISSUER, NODE_ENV, REDIS_URL, USE_REDIS } = process.env;
 
 const ISSUER_PORT = PORT || "3000";
+const IS_PRODUCTION: boolean = NODE_ENV === "production";
 
 export const config: IConfig = {
   ISSUER: ISSUER || `http://localhost:${ISSUER_PORT}`,
   PORT: parseInt(ISSUER_PORT, 10),
-  IS_PRODUCTION: NODE_ENV === "production",
+  IS_PRODUCTION,
+  REDIS_URL: REDIS_URL || "redis://redis:6379/",
+  USE_REDIS: IS_PRODUCTION || USE_REDIS === "true",
 };
