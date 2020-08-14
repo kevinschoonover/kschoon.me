5dbdb278e26e23b8af285d8cb0944801d225a352
Kevin Schoonover <me@kschoon.me>
2020-08-14 11:46:04 -0700

    Fix typeorm migration:run in db.sh not finding configuration

--- identity/src/index.ts
+++ identity/src/index.ts
@@ -2,6 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 
 import { createConnection } from "typeorm";
 import identityHandler from "./handlers/identity";
+import connectionOptions from "./lib/db";
 
 import { config } from "./config";
 
@@ -16,29 +17,7 @@ async function startServer() {
     ...identityHandler.handler,
   });
 
-  const connection = await createConnection({
-    type: "postgres",
-    url: config.POSTGRES_URL || "postgres://postgres:postgres@db:5432",
-    logging: ["error", "warn"],
-    logger: "advanced-console",
-    cache: true,
-    // See src/main.ts as to why these are all false
-    // Should we automatically synchronize our database?
-    synchronize: false,
-    // Run migrations automatically,
-    migrationsRun: false,
-    // Should we automatically drop the entire database on start?
-    dropSchema: false,
-    entities: [`${__dirname}/resources/**/index{.js,.ts}`],
-    migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
-    subscribers: [`${__dirname}/subscribers/**/*{.js.ts}`],
-    cli: {
-      entitiesDir: `${__dirname}/entities`,
-      migrationsDir: `${__dirname}/migrations`,
-      subscribersDir: `${__dirname}/subscribers`,
-    },
-    // ssl: IS_PROD ? true : false
-  });
+  const connection = await createConnection(connectionOptions);
   await connection.runMigrations();
 
   // define the host/port for server
