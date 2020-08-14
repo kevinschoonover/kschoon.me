75498454c7d376c9cffa1e829ea6b03aa593ebb8
Kevin Schoonover <me@kschoon.me>
2020-08-14 11:46:04 -0700

    Fix typeorm migration:run in db.sh not finding configuration

--- identity/db.sh
+++ identity/db.sh
@@ -1,10 +1,10 @@
 docker stop db && docker rm db
 docker run -d -e POSTGRES_PASSWORD=postgres --name db -p 5432:5432 --tmpfs /var/lib/postgresql/data:rw postgres:12
 sleep 3
-yarn typeorm migration:run
+yarn typeorm migration:run --config ./src/lib/db.ts
  docker exec -it db psql -U postgres -c \
      "INSERT INTO \"user\"(email, \"phoneNumber\", \"givenName\", \"familyName\", locale, \"totpSecret\", \"totpRecovery\") VALUES 
-         ('me@kschoon.me', '+12144497919', 'Kevin', 'Schoonover', 'test', '1', '1'), 
-         ('me2@kschoon.me', '+12154497919', 'Kevin', 'Schoonover', 'test', '1', '1')"
+         ('testing@kschoon.me', '+12148431643', 'Kevin', 'Schoonover', 'test', '1', '1'), 
+         ('testing2@kschoon.me', '+18432341324', 'Kevin', 'Schoonover', 'test', '1', '1')"
  docker exec -it db psql -U postgres -c \
      "SELECT * FROM \"user\""
---
+++ identity/src/lib/db.ts
@@ -0,0 +1,26 @@
+import { ConnectionOptions } from "typeorm";
+import { config } from "../config";
+
+export = {
+  type: "postgres",
+  url: config.POSTGRES_URL || "postgres://postgres:postgres@db:5432",
+  logging: ["error", "warn"],
+  logger: "advanced-console",
+  cache: true,
+  // See src/main.ts as to why these are all false
+  // Should we automatically synchronize our database?
+  synchronize: false,
+  // Run migrations automatically,
+  migrationsRun: false,
+  // Should we automatically drop the entire database on start?
+  dropSchema: false,
+  entities: [`${__dirname}/../resources/**/index{.js,.ts}`],
+  migrations: [`${__dirname}/../migrations/**/*{.js,.ts}`],
+  subscribers: [`${__dirname}/../subscribers/**/*{.js.ts}`],
+  cli: {
+    entitiesDir: `${__dirname}/../entities`,
+    migrationsDir: `${__dirname}/../migrations`,
+    subscribersDir: `${__dirname}/../subscribers`,
+  },
+  // ssl: IS_PROD ? true : false
+} as ConnectionOptions;
